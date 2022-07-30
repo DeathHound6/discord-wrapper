d40f99076e4218a4e332017e7d9a10cce2ec5227
DeathHound <acraig176@hotmail.com>
2022-07-30 12:01:37 +0100

    Create ws utils

--- modules/Lib/src/managers/CDN.js
+++ modules/Lib/src/managers/CDN.js
@@ -11,7 +11,7 @@ export class CDNManager {
      * @returns {String} The user's banner URL
      */
     getAvatarURL(id, hash, options) {
-        let format = options?.type || "webp"
+        let format = options?.type || "webp";
         if (hash.substring(0, 2) == "a_")
             format = "gif";
         return `${this.baseURL}/avatars/${id}/${hash}.${format}?size=${options?.size || 512}`;
@@ -21,7 +21,7 @@ export class CDNManager {
      * Get a users banner URL
      * @param {Snowflake} id The user's id
      * @param {String} hash The banner hash
-     * @param {AvatarURLOptions} The options for the URL
+     * @param {AvatarURLOptions} options The options for the URL
      * @returns {String} The user's banner URL
      */
     getBannerURL(id, hash, options) {
--- modules/Lib/src/ws.js
+++ modules/Lib/src/ws.js
@@ -1,2 +1,33 @@
 import ws from "ws";
 
+let erl = null;
+try {
+    erl = await import('erlpack')
+    if (!erl.pack) erl = null;
+} catch {}
+const decoder = new TextDecoder();
+export const encoding = erl ? "etf" : "json";
+export const pack = erl ? erl.pack : JSON.stringify;
+
+export function create(gateway, query = {}, ...args) {
+    const [g, q] = gateway.split("?");
+    query.encoding = encoding;
+    query = new URLSearchParams(query);
+    if (q) new URLSearchParams(q).forEach((v, k) => query[k] = v);
+    return new ws(`${g}?${query}`, ...args);
+}
+
+export function unpack(data, type) {
+    if (encoding == "json" || type == "json") {
+        if (typeof data != "string")
+            data = decoder.decode(data);
+        return JSON.parse(data);
+    }
+    if (!Buffer.isBuffer(data)) data = Buffer.from(new Uint8Array(data));
+    return erl.unpack(data);
+}
+
+export const CONNECTING = ws["CONNECTING"];
+export const OPEN = ws["OPEN"];
+export const CLOSING = ws["CLOSING"];
+export const CLOSED = ws["CLOSED"];
